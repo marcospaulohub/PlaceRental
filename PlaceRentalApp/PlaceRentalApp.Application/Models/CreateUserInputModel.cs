acd169878592a3e8b8d0927181fe8091dfabf30b
Marcos Paulo <marcos.poo@gmail.com>
2025-03-04 20:41:48 -0300

    refactor: Remove o set privado.

--- PlaceRentalApp/PlaceRentalApp.Application/Models/CreateUserInputModel.cs
+++ PlaceRentalApp/PlaceRentalApp.Application/Models/CreateUserInputModel.cs
@@ -5,7 +5,7 @@
         public string FullName { get; set; }
         public string Email { get; set; }
         public DateTime BirtDate { get; set; }
-        public string Password { get; private set; }
-        public string Role { get; private set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
     }
 }
