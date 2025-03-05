9c14bc630982c340c22b4c55666e55d13ce620e2
Marcos Paulo <marcos.poo@gmail.com>
2025-03-04 21:18:51 -0300

    feat: Adiciona a busca por email e hash no UserRepository.

--- PlaceRentalApp/PlaceRentalApp.Core/IRepositories/IUserRepository.cs
+++ PlaceRentalApp/PlaceRentalApp.Core/IRepositories/IUserRepository.cs
@@ -5,6 +5,7 @@ namespace PlaceRentalApp.Core.IRepositories
     public interface IUserRepository
     {
         User? GetById(int id);
+        User? GetByLoginAndHash(string email, string hash);
         int AddUser(User user);
     }
 }
